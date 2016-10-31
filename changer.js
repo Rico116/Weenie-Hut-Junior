46aef5f6da1964103205d6c014cd23184b4eee2a
Justin Rico <jrico.sh@mysummitps.org>
2016-10-31 22:00:42 +0000

    Created color changing checkpoint

---
+++ changer.js
@@ -0,0 +1,12 @@
+var newBGColor = document.getElementById("newBGColor");
+var colorChangeButton = document.getElementById("colorChangeButton");
+
+/* Event Listener for Button.
+    Sets the body's background color to the user provided value */
+
+colorChangeButton.addEventListener("click", function(){
+    document.body.style.backgroundColor = newBGColor.value;
+
+})
+
+
